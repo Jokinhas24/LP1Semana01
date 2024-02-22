c9b552020795ff0aed442ebf45b4977aebbc593c
João Nogueira <joaonogueira.art@gmail.com>
2024-02-22 23:35:51 +0000

    Added Intereger Variables

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Integer Variables
+            int i = 4;
+            uint ui = 5U;
+            long l = 3L;
+            ulong ul = 5UL;
+
+            Console.WriteLine(i);
+            Console.WriteLine(ui);
+            Console.WriteLine(l);
+            Console.WriteLine(ul);
+        }
+    }
+}
