5ba9312abbc8294da321069f53a4b27c65c704e0
João Nogueira <joaonogueira.art@gmail.com>
2024-02-22 23:59:37 +0000

    Added Char Variables

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -11,11 +11,22 @@ namespace VariousTypes
             uint ui = 5U;
             long l = 3L;
             ulong ul = 5UL;
-
+            
+            //Prints Integer Variables on the Console
             Console.WriteLine(i);
             Console.WriteLine(ui);
             Console.WriteLine(l);
             Console.WriteLine(ul);
+
+            //Char Variables
+            char heart = '\u2665';
+            char cloud = '\u2601';
+            char arrow = '\u27BC';
+
+            //Prints Char Variables on the Console
+            Console.WriteLine(heart);
+            Console.WriteLine(cloud);
+            Console.WriteLine(arrow);
         }
     }
 }
