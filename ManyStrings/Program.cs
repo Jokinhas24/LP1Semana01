dc1b0dcc2d5aaa84323f0c35c70e3a5256b72ef6
João Nogueira <joaonogueira.art@gmail.com>
2024-02-23 01:55:59 +0000

    Added xx and ii Variables and respective alterations

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -6,6 +6,10 @@ namespace ManyStrings
     {
         static void Main(string[] args)
         {
+            //Ex6 Variables
+            double xx = 1.23456;
+            int ii = 19;
+
             //Strings Build
             string cardsymbols = "\u2660 \u2665 \u2663 \u2666";
             string train = "I'm a cho cho train \"ChhOOoO ChOOooOO\"";
@@ -15,6 +19,9 @@ namespace ManyStrings
             Console.WriteLine(cardsymbols);
             Console.WriteLine(train);
             Console.WriteLine(dragon);
+
+            //Just for better reading
+            Console.WriteLine("\n");
 
             //Variable Declaration
             string a = "aaaaaaaaaaaah";
@@ -29,12 +36,31 @@ namespace ManyStrings
             string heartbro = $"I'm {heart}broken";
             string notfour = $"This number is not four: {four + five}";
 
-            //Prints all strings
+            //Prints all new strings
             Console.WriteLine(scream);
             Console.WriteLine(heartbro);
             Console.WriteLine(notfour);
 
+            //Just for better reading
+            Console.WriteLine("\n");
 
+            //Prints xx in
+            //Real with 2 decimals
+            string xx_f = $"{xx:f2}";
+            Console.WriteLine(xx_f);
+
+            //Percent with 1 decimal
+            string xx_p = $"{xx:p1}";
+            Console.WriteLine(xx_p);
+
+            //Prints ii in
+            //Hexadecimal
+            string ii_h = $"{ii:x}";
+            Console.WriteLine(ii_h);
+
+            //Coin
+            string ii_c = $"{ii:c}";
+            Console.WriteLine(ii_c);
         }
     }
 }
