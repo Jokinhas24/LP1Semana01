7782c325de1b76e9e692dc0a2af22cd1a55c7b35
João Nogueira <joaonogueira.art@gmail.com>
2024-02-23 02:06:22 +0000

    Added a way to ask for number and print number

---
+++ AskForInput/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AskForInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask for Number
+            Console.WriteLine("Please insert a Integer Number:");
+            string n = Console.ReadLine();
+            int numb = int.Parse(n);
+
+            //Printing that number
+            Console.WriteLine(numb);
+        }
+    }
+}
