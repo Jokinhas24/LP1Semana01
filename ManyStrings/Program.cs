ecb17a1afd52862b381961280cda2f3bda0755e0
João Nogueira <joaonogueira.art@gmail.com>
2024-02-23 01:05:48 +0000

    Added Strings

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Strings Build
+            string cardsymbols = "\u2660 \u2665 \u2663 \u2666";
+            string train = "I'm a cho cho train \"ChhOOoO ChOOooOO\"";
+            string dragon = "I'm a flying dragon,\n FLAP FLAP FLAP";
+
+            //Printing Strings
+            Console.WriteLine(cardsymbols);
+            Console.WriteLine(train);
+            Console.WriteLine(dragon);
+
+        }
+    }
+}
